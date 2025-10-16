31abe7578e498c488fecb43147a64cc7b20912f3
liamfortin <liam.e.fortin@gmail.com>
2025-10-16 16:25:50 +0000

    Added color, removed some text.

--- main.js
+++ main.js
@@ -2,14 +2,37 @@ document.getElementById("currencyDisplay").innerHTML = "You've got zero coins! C
 var coins = 0;
 function incrementMoney () {
     coins++;
-    document.getElementById("currencyDisplay").innerHTML= "Coins: " + coins;
+    updateDisplay();
     if (coins >= 25) {
-        document.getElementById('moneyButton2').style.display=('block')
+        document.getElementById('moneyButton2').style.display=('block');
     }
 }
 function doubleMoney() {
     if (coins >= 5) {
         coins*=2;
-        document.getElementById("currencyDisplay").innerHTML= "Coins: " + coins;
+        updateDisplay();
     }
 }
+
+function updateDisplay() {
+    if (coins > 100) {
+        document.getElementById("currencyDisplay").innerHTML= "Coins: " + coins + " (-" + Math.floor(coins / (2 * Math.log10(coins))) + "/s from taxes!)";
+    } else {
+        document.getElementById("currencyDisplay").innerHTML= "Coins: " + coins;
+    }
+
+}
+
+function taxMoney() {
+    if (coins > 100) {
+        var tax = Math.floor(coins / (2 * Math.log10(coins)));
+        if (coins - tax <= 100) {
+            coins = 100;
+        } else {
+            coins -= tax;
+        }
+        updateDisplay();
+    }
+    setTimeout(taxMoney, 1000); 
+}
+taxMoney();
